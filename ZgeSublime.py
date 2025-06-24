5f958572eafd5dc35fbb79b81502641d66e903e8
Txori <3765943+Txori@users.noreply.github.com>
2025-06-24 11:33:13 +0200

    Update ZgeSublime.py
    Added a function to fold/unfold data tags such as BitmapFile|FileEmbedded|Icon|MeshData

--- ZgeSublime.py
+++ ZgeSublime.py
@@ -44,3 +44,64 @@ class ZgeRunProjectCommand(sublime_plugin.TextCommand):
             subprocess.Popen([exe_path])
         else:
             sublime.error_message("Build failed: EXE not created.")
+
+class ZgeFoldDataCommand(sublime_plugin.TextCommand):
+    """
+    Sublime Text plugin to toggle the collapsed (folded) state of the
+    content within <MeshImport> and <BitmapFromFile> XML tags.
+
+    If the content is currently unfolded, it will fold it, displaying '...'.
+    If the content is currently folded, it will unfold it, revealing the content.
+    """
+    def run(self, edit):
+        # 1. Get the entire content of the current view.
+        content = self.view.substr(sublime.Region(0, self.view.size()))
+
+        # 2. Define the regular expression pattern to find the tags.
+        #    This regex remains the same as it correctly identifies the start and end
+        #    of the content that needs to be folded/unfolded.
+        pattern = re.compile(
+            r'(<(BitmapFile|FileEmbedded|Icon|MeshData)[^>]*>)(.*?)(</\2>)',
+            re.DOTALL
+        )
+
+        # 3. Initialize a list to store the regions that are candidates for folding/unfolding.
+        regions_to_toggle = []
+
+        # 4. Iterate over all matches found by the regex.
+        for match in pattern.finditer(content):
+            # Extract the start and end positions (character offsets) of the matched groups.
+            open_tag_end = match.end(1)    # Position right after '>' of the opening tag.
+            close_tag_start = match.start(4) # Position right before '<' of the closing tag.
+
+            # Create a sublime.Region object for the content to be toggled.
+            fold_region = sublime.Region(open_tag_end, close_tag_start)
+
+            # Only consider regions with actual content to toggle.
+            if fold_region.begin() < fold_region.end():
+                regions_to_toggle.append(fold_region)
+
+        # If no relevant tags are found, there's nothing to do.
+        if not regions_to_toggle:
+            sublime.status_message("No data tags found to toggle.")
+            # print("No regions found to toggle.") # Debug print
+            return # Exit the function early
+
+        # 5. Determine the current state of the folds.
+        #    We check the first identified region. If it's folded, we assume the intent
+        #    is to unfold all. Otherwise, the intent is to fold all.
+        #    This is a simplification; for more robust toggling, you might check if *all*
+        #    are folded, or only toggle individually if some are folded and some aren't.
+        #    For this use case (collapsing/expanding entire blocks), checking one is usually sufficient.
+        first_region_is_folded = self.view.is_folded(regions_to_toggle[0])
+
+        if first_region_is_folded:
+            # If the first region is folded, unfold all regions.
+            self.view.unfold(regions_to_toggle)
+            sublime.status_message("Unfolded {} data tags.".format(len(regions_to_toggle)))
+            # print("Successfully unfolded {} regions.".format(len(regions_to_toggle))) # Debug print
+        else:
+            # If the first region is not folded, fold all regions.
+            self.view.fold(regions_to_toggle)
+            sublime.status_message("Folded {} data tags.".format(len(regions_to_toggle)))
+            # print("Successfully folded {} regions.".format(len(regions_to_toggle))) # Debug print
